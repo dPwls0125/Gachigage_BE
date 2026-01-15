5dcea16b32f2eb724b69397a2e4a444e7311375c
inwoo <ninw0205@gmail.com>
2026-01-15 15:27:30 +0900

    [fix] ApiExceptionHandler, ErrorCode에 공용 에러 로직 추가 (#3)

--- src/main/java/com/gachigage/global/error/ApiExceptionHandler.java
+++ src/main/java/com/gachigage/global/error/ApiExceptionHandler.java
@@ -1,8 +1,16 @@
 package com.gachigage.global.error;
 
+import java.util.stream.Collectors;
+
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.gachigage.global.ApiResponse;
 
@@ -10,7 +18,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(CustomException.class)
 	public ResponseEntity<ApiResponse<Void>> handleCustomException(CustomException ce) {
@@ -26,5 +34,32 @@ public class ApiExceptionHandler {
 		ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
 
 		return ResponseEntity.status(errorCode.getHttpStatus()).body(ApiResponse.fail(errorCode));
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+		HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		log.error("Unexpected Error: ", ex);
+
+		String detailedMessage = ex.getBindingResult()
+			.getAllErrors()
+			.stream()
+			.map(DefaultMessageSourceResolvable::getDefaultMessage)
+			.collect(Collectors.joining(", "));
+		Object body = ApiResponse.fail(ErrorCode.from(status), detailedMessage);
+
+		return handleExceptionInternal(ex, body, headers, status, request);
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+		HttpStatusCode statusCode, WebRequest request) {
+		log.error("Unexpected Error: ", ex);
+
+		if (body == null) {
+			ErrorCode errorCode = ErrorCode.from(statusCode);
+			body = ApiResponse.fail(errorCode);
+		}
+		return super.handleExceptionInternal(ex, body, headers, statusCode, request);
 	}
 }
--- src/main/java/com/gachigage/global/error/ErrorCode.java
+++ src/main/java/com/gachigage/global/error/ErrorCode.java
@@ -1,6 +1,7 @@
 package com.gachigage.global.error;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -8,10 +9,31 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "U001", "유저를 찾을 수 없습니다."), INTERNAL_SERVER_ERROR(
-		HttpStatus.INTERNAL_SERVER_ERROR, "E999", "서버 내부에 오류가 발생했습니다.");
+	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "U001", "유저를 찾을 수 없습니다."),
+
+	// 표준 에러
+	INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "C001", "잘못된 입력값입니다."),
+	UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "C002", "인증되지 않은 사용자입니다."),
+	ACCESS_DENIED(HttpStatus.FORBIDDEN, "C003", "접근 권한이 없습니다."),
+	RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "C004", "요청하신 리소스를 찾을 수 없습니다."),
+	METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "C005", "지원하지 않는 HTTP 메서드입니다."),
+	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "C006", "서버 내부 오류가 발생했습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String code;
 	private final String message;
+
+	public static ErrorCode from(HttpStatusCode status) {
+		if (status instanceof HttpStatus httpStatus) {
+			return switch (httpStatus) {
+				case BAD_REQUEST -> INVALID_INPUT_VALUE;
+				case UNAUTHORIZED -> UNAUTHORIZED;
+				case FORBIDDEN -> ACCESS_DENIED;
+				case NOT_FOUND -> RESOURCE_NOT_FOUND;
+				case METHOD_NOT_ALLOWED -> METHOD_NOT_ALLOWED;
+				default -> INTERNAL_SERVER_ERROR;
+			};
+		}
+		return INTERNAL_SERVER_ERROR;
+	}
 }
