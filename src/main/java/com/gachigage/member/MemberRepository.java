3f72cdb274a8006e4c5e8e86c2d106ce911e69db
어진욱 <djxl3606@gmail.com>
2026-01-28 00:01:01 +0900

    Revert "충돌부분 제거"
    This reverts commit bd3f8409069bee192e95346609f146eb51bf0b4f.

--- src/main/java/com/gachigage/member/MemberRepository.java
+++ src/main/java/com/gachigage/member/MemberRepository.java
@@ -8,7 +8,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 	Optional<Member> findMemberByEmail(String email);
 
 
-
+	Optional<Member> findByOauthId(Long oauthId);
 
 
 }
