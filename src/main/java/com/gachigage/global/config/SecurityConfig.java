0d130887b59278f635063d0e717c7756df6c75af
inwoo <ninw0205@gmail.com>
2026-02-28 20:09:10 +0900

    [chore] Dev 버전 시 로컬 프론트 cors 매핑 추가

--- src/main/java/com/gachigage/global/config/SecurityConfig.java
+++ src/main/java/com/gachigage/global/config/SecurityConfig.java
@@ -1,7 +1,12 @@
 package com.gachigage.global.config;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Optional;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,6 +15,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.gachigage.global.login.service.CustomOAuth2UserService;
 
@@ -25,7 +33,15 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http,
+		Optional<CorsConfigurationSource> corsConfigurationSource,
 		CustomAuthenticationEntryPoint customAuthenticationEntryPoint) throws Exception {
+
+		if (corsConfigurationSource.isPresent()) {
+			http.cors(cors -> cors.configurationSource(corsConfigurationSource.get()));
+		} else {
+			http.cors(AbstractHttpConfigurer::disable); // prod 등 빈이 없는 경우 CORS 비활성화
+		}
+
 		http.csrf(AbstractHttpConfigurer::disable)
 			.formLogin(AbstractHttpConfigurer::disable)
 			.httpBasic(AbstractHttpConfigurer::disable)
@@ -50,5 +66,19 @@ public class SecurityConfig {
 			.addFilterBefore(new JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
+	}
+
+	@Bean
+	@Profile("dev")
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOriginPatterns(Collections.singletonList("http://localhost:3000"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		configuration.setAllowedHeaders(Collections.singletonList("*"));
+		configuration.setAllowCredentials(true);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 }
